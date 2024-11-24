5ada95d6cd502e27a5a1cb60ef975275ba6ce318
SvetlinGarabedyan <svetlingarabedyan@yahoo.com>
2024-11-24 02:07:53 +0100

    main components

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { PostsComponent } from './posts/posts.component';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [],
+  imports: [PostsComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.css'
 })
