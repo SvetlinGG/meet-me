44e659d1d85cc46c8f3bb4ea5a372e24b4ea476a
SvetlinGarabedyan <svetlingarabedyan@yahoo.com>
2024-11-25 21:31:41 +0100

    Theme list

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { PostsComponent } from './posts/posts.component';
+import { CatalogListComponent } from '../theme/themes-list/themes-list.component';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [PostsComponent],
+  imports: [PostsComponent, CatalogListComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.css'
 })
---
+++ src/app/theme/themes-list/themes-list.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { Theme } from '../../types/theme';
+import { ApiService } from '../../api.service';
+
+@Component({
+  selector: 'app-catalog-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './themes-list.component.html',
+  styleUrl: './themes-list.component.css'
+})
+export class CatalogListComponent implements OnInit{
+  themes: Theme[] = [];
+  isLoading = true;
+  constructor(private apiService: ApiService){
+
+  }
+  ngOnInit(){
+    this.apiService.getThemes().subscribe((themes) => {
+      console.log(themes);
+      this.themes = themes;
+      this.isLoading = false;
+      
+    });
+  }
+}
